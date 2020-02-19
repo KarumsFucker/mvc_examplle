eae67d8f0a811c13909b07e9d7c1f649664a19f9
MyName <MyName@myemail.com>
2020-02-19 10:29:23 +0200

    Первый комментарий

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include_once 'model/Model.php';//Model
+include_once 'controller/controller.php';
+include_once 'route/routing.php';
+echo $responce;
---
+++ view/error404.php
@@ -0,0 +1,10 @@
+<?php ob_start() ?>
+<h2>404 ошибка</h2>
+<article>
+	<p>
+		По запрашиваему URL страница не найдена
+	</p>
+</article>
+
+<?php $content = ob_get_clean(); ?>
+<?php include "view/templates/layout.php";
